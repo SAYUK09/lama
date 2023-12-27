90f0c38cb44649009063f23951f92dde60fe40d6
Sayuri <sayurikamble123@gmail.com>
2023-12-27 21:11:44 +0530

    Mantine UI library setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,9 @@ import "./globals.css";
 import Image from "next/image";
 import logo from "../../public/assests/directright.svg";
 import { GlobalContextProvider } from "@/context/globalContext";
+import "@mantine/core/styles.css";
 
+import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,13 +17,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ColorSchemeScript />
         <nav className="p-4 m-2">
           <div className="flex items-center text-primary font-bold text-3xl">
             <Image width={50} height={50} src={logo} alt="nav logo"></Image>
             <p>LAMA.</p>
           </div>
         </nav>
-        <GlobalContextProvider>{children}</GlobalContextProvider>
+        <MantineProvider defaultColorScheme="light">
+          <GlobalContextProvider>{children}</GlobalContextProvider>
+        </MantineProvider>
       </body>
     </html>
   );
